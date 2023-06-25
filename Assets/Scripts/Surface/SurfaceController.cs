0276199da70c4c24f50179eae2b5fa4f4cf10cbf
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-26 04:33:55 +1000

    Created Surface class and add trigger to update the player controller with the surface type they are on

---
+++ Assets/Scripts/Surface/SurfaceController.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ESurfaceType
+{
+    REGULAR = 0,
+    SAND = 1,
+    ICE = 2
+}
+
+public class SurfaceController : MonoBehaviour
+{
+    [SerializeField] private ESurfaceType _SurfaceType;
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check that a player has entered the trigger
+        if (other.CompareTag("Player"))
+        {
+            // Get reference to the player controller
+            PlayerController player = other.GetComponent<PlayerController>();
+            // Validate the player controller and set the On sourface property
+            if (player)
+                player.OnSurface = this._SurfaceType;
+        }
+    }
+}
