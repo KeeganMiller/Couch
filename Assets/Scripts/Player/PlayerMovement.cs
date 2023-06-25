733bc96d55bdd091d36f3853dcaa3ddd211b34cc
EilexInteractive <95119915+EilexInteractive@users.noreply.github.com>
2023-06-26 03:00:45 +1000

    Added movement and change of rotation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,15 +5,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement Settings")]
     [SerializeField] protected float _MovementSpeed;              // Speed the player object will move at
+    [SerializeField] protected float _RotationSpeed;                // Speed at which the character will rotation at
+
+    [Header("Player Inputs")] 
+    [SerializeField] protected PlayerInput _Input;
+
 
     [Header("Components")] 
-    [SerializeField] protected Rigidbody _RBody; 
+    [SerializeField] protected CharacterController _CharController;                    // Reference to the Character Controller
     
     protected virtual void Awake()
     {
-        if (!_RBody)
-            _RBody = this.GetComponent<Rigidbody>();
+        // Get reference to the character controller
+        if (!_CharController)
+            _CharController = this.GetComponent<CharacterController>();
+
+        _Input = new PlayerInput();
+    }
+
+    protected void OnEnable()
+    {
+        _Input.Enable();
+    }
+
+    protected void OnDisable()
+    {
+        _Input.Disable();
     }
 
     protected void FixedUpdate()
@@ -23,6 +42,24 @@ public class PlayerMovement : MonoBehaviour
 
     private void HandleMovement()
     {
-        
+        // Validate the rigidbody before handling movement
+        if (!_CharController)
+            return;
+
+        var movementInput = _Input.Player.Move.ReadValue<Vector2>();                    // Get the input value as vector 2
+        var movementValue = new Vector3(movementInput.x, 0f, movementInput.y);      // Create the movement value
+
+        if (movementValue.normalized.magnitude > 0.1f)
+        {
+            var finalMovement = movementValue * (_MovementSpeed * Time.deltaTime);          // Calculate final movement direction
+            _CharController.Move(finalMovement);                // Apply movement to the character controller
+            
+            Quaternion toRotation = Quaternion.LookRotation(finalMovement, Vector3.up);             // Get where we want to rotate the character to
+            // Apply rotation smoothly
+            transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, toRotation, (_RotationSpeed * Time.deltaTime));
+        }
+
+
     }
 }
